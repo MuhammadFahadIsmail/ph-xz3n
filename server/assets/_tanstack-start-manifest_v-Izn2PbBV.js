ab5170bc6a0434beefbceb79a13efaceb8d60321
Muhammad Fahad Ismail <MuhammadFahadIsmail@users.noreply.github.com>
2026-05-05 17:50:32 +0500

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Izn2PbBV.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/afsha/Downloads/PHIXZEN/phixzen/phixzen-zen-clarity-care-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/ph-xz3n/assets/index-CXdQYVLB.js"] }, "/": { filePath: "C:/Users/afsha/Downloads/PHIXZEN/phixzen/phixzen-zen-clarity-care-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/ph-xz3n/assets/index-Djqqr09F.js"] } }, clientEntry: "/ph-xz3n/assets/index-CXdQYVLB.js" });
+export {
+  tsrStartManifest
+};
